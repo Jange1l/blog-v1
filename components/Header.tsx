4b013c5bfe1724203758547f9fd4ec64c4d65c62
Juan Suarez <juansuarez@Juans-MBP.lan>
2024-05-10 21:42:43 -0400

    Adding new post understanding text embeddings

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,13 +12,8 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            {/* <div className="mr-3">
-              <Logo />
-            </div> */}
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
+              <div className="h-6 text-2xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
             ) : (
               siteMetadata.headerTitle
             )}
